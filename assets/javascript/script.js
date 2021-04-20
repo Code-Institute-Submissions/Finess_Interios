cc157c9edcd43410977fa8d1b24e9ab4b5971767
Francisco Sabino <fdasabino@gmail.com>
2021-04-20 21:01:58 +0200

    version 3.0 added hamburger menu and some new trasition effect

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,3 +8,12 @@ function playVideo(file) {
   myVideo.src = file;
   videoPlayer.style.display = "block";
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
