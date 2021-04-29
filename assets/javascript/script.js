6ca0e3f582f222e7265ca36cec8337c1f62f4140
Francisco Sabino <fdasabino@gmail.com>
2021-04-29 02:31:57 +0200

    good progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,3 @@
-// Function expression to select elements.
-const selectElement = (s) => document.querySelector(s);
-
-//Open the menu on click
-selectElement(".open").addEventListener("click", () => {
-  selectElement(".nav-list").classList.add("active");
-});
-
-//Close the menu on click
-selectElement(".close").addEventListener("click", () => {
-  selectElement(".nav-list").classList.remove("active");
-});
-
 var videoPlayer = document.getElementById("videoPlayer");
 var myVideo = document.getElementById("myVideo");
 
